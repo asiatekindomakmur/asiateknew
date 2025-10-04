a9d2b095e63797b93cb9025257098e14dc5ef645
amanrent <amanrentc@gmail.com>
2025-10-04 23:12:46 +0700

    Commit

--- admin/artikel.php
+++ admin/artikel.php
@@ -97,7 +97,7 @@ $result = $conn->query("SELECT * FROM artikel ORDER BY created_at DESC");
   <div class="main-content">
     <div class="d-flex justify-content-between align-items-center mb-4">
       <h3 class="fw-semibold text-primary"><i class="fa-solid fa-file-alt me-2"></i> Manajemen Artikel</h3>
-      <a href="article_tambah.php" class="btn btn-primary"><i class="fa-solid fa-plus me-1"></i> Tambah Artikel</a>
+      <a href="artikel_tambah.php" class="btn btn-primary"><i class="fa-solid fa-plus me-1"></i> Tambah Artikel</a>
     </div>
 
     <div class="card p-3">
