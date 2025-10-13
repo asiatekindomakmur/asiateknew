2c15d2bfe1195795d5a2afa09e12292ef75de711
amanrent <amanrentc@gmail.com>
2025-10-13 16:09:40 +0700

    Commit

--- footer.php
+++ footer.php
@@ -39,15 +39,7 @@
     <!-- Google Maps Box -->
     <div class="footer-section">
       <div class="google-map-container" style="margin-top: 20px">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=..."
-          width="100%"
-          height="200"
-          style="border: 0"
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade">
-        </iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.6806696924154!2d106.77030197355363!3d-6.173490760493029!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f7d546233a65%3A0x12f0fd11a90e435!2sPT.%20Asiatek%20Indo%20Makmur!5e0!3m2!1sid!2sid!4v1760346539517!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
     </div>
   </div>
