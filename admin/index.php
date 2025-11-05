6c8e9d2a632c7915bd260210845d02f690ac5057
amanrent <amanrentc@gmail.com>
2025-11-05 11:54:27 +0700

    Commit

--- admin/index.php
+++ admin/index.php
@@ -144,6 +144,9 @@ try {
       </div>
     </div>
   </div>
-
+  <!-- JS -->
+  <script src="js/admin.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+  
 </body>
 </html>
